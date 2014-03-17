c2ded6e7a4a241391d2faab0dd7814e3ae69e39e
Aaron Smith <asmith1810@gmail.com>
2014-03-17 01:02:21 -0500

    Changed font and removed default directory

--- init.el
+++ init.el
@@ -40,8 +40,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#161A1F" :foreground "#DEDEDE" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "Droid Sans Mono")))))
 
 ;; set default directory for C-x C-f
-(setq default-directory "/Users/aaron/Documents/Code/")
+;; (setq default-directory "/Users/aaron/Documents/Code/")
+
 (put 'dired-find-alternate-file 'disabled nil)
