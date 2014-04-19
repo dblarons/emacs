b0ea16671d3883fe49e92e8b1af44fa44b060b1c
Aaron Smith <asmith1810@gmail.com>
2014-04-19 18:13:19 -0500

    Added toggle-window-split function to user.el

--- user.el
+++ user.el
@@ -122,6 +122,35 @@
 (require 'autopair)
 (autopair-global-mode) ;; enable autopair in all buffers
 
+;; toggles the window split from verticle to horizontal and vice versa
+;; taken from the Emacs Rocks guy
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+;; binds toggle-window-split to C-c w
+(global-set-key (kbd "C-c w") 'toggle-window-split)
 
 ;; Io Mode
 (require 'io-mode)
